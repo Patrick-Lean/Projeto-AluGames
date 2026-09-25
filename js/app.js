9358e05ed6d467234b278bd0c2c954789a396977
Patrick Leandro <patrickleandrocontato@outlook.com>
2026-09-25 16:13:53 -0300

    Ajustado a identação do codigo

--- js/app.js
+++ js/app.js
@@ -1,27 +1,27 @@
-function alterarStatus(id){
+function alterarStatus(id) {
     let jogoClicado = document.getElementById(`game-${id}`);
     let imagem = jogoClicado.querySelector('.dashboard__item__img');
     let botao = jogoClicado.querySelector('.dashboard__item__button');
-    if(imagem.classList.contains('dashboard__item__img--rented')){
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
         let confirmação = confirm("Confirme se deseja devolver o jogo");
-        if(confirmação){
+        if (confirmação) {
             imagem.classList.remove('dashboard__item__img--rented');
             botao.classList.remove('dashboard__item__button--return');
-            botao.innerHTML="Alugar";
+            botao.innerHTML = "Alugar";
         }
-    }else{
+    } else {
         imagem.classList.add('dashboard__item__img--rented');
         botao.classList.add('dashboard__item__button--return');
-        botao.innerHTML="Devolver";
+        botao.innerHTML = "Devolver";
     }
     quantJogosAlugados()
 }
 
-function quantJogosAlugados(){
-    let jogosAlugados=0;
-    for(let i=1;i<4;i++){
+function quantJogosAlugados() {
+    let jogosAlugados = 0;
+    for (let i = 1; i < 4; i++) {
         let jogo = document.getElementById(`game-${i}`).querySelector('.dashboard__item__button');
-        if(jogo.classList.contains('dashboard__item__button--return')){
+        if (jogo.classList.contains('dashboard__item__button--return')) {
             jogosAlugados = jogosAlugados + 1;
         }
     }
