30c89c9ee0b667ad1c1c4b619ff036a56da7f99e
Patrick Leandro <patrickleandrocontato@outlook.com>
2026-09-25 16:12:56 -0300

    Adicionado no log a quantidade de itens alugados

--- js/app.js
+++ js/app.js
@@ -14,4 +14,16 @@ function alterarStatus(id){
         botao.classList.add('dashboard__item__button--return');
         botao.innerHTML="Devolver";
     }
+    quantJogosAlugados()
+}
+
+function quantJogosAlugados(){
+    let jogosAlugados=0;
+    for(let i=1;i<4;i++){
+        let jogo = document.getElementById(`game-${i}`).querySelector('.dashboard__item__button');
+        if(jogo.classList.contains('dashboard__item__button--return')){
+            jogosAlugados = jogosAlugados + 1;
+        }
+    }
+    console.log(jogosAlugados);
 }
