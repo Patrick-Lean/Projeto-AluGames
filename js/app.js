6e401e516f98e2f310666504286edd27eb81b7d4
Patrick Leandro <patrickleandrocontato@outlook.com>
2026-09-25 15:45:48 -0300

    If desnecessário retriado

--- js/app.js
+++ js/app.js
@@ -4,14 +4,10 @@ function alterarStatus(id){
     let botao = jogoClicado.querySelector('.dashboard__item__button');
     if(imagem.classList.contains('dashboard__item__img--rented')){
         imagem.classList.remove('dashboard__item__img--rented');
-    }else{
-        imagem.classList.add('dashboard__item__img--rented');
-    }
-
-    if(botao.classList.contains('dashboard__item__button--return')){
         botao.classList.remove('dashboard__item__button--return');
         botao.innerHTML="Alugar";
     }else{
+        imagem.classList.add('dashboard__item__img--rented');
         botao.classList.add('dashboard__item__button--return');
         botao.innerHTML="Devolver";
     }
